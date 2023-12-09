99492792a63fb0f21dab576fbc96a799f4c51e4c
DominicWuest <dominic.wuest@hotmail.com>
2023-12-09 13:53:11 +0100

    Added screenlayout scripts

---
+++ dot_screenlayout/private_executable_home.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+xrandr --output eDP --primary --mode 1920x1200 --pos 0x924 --rotate normal --output HDMI-A-0 --off --output DisplayPort-0 --off --output DisplayPort-1 --off --output DisplayPort-2 --off --output DisplayPort-3 --off --output DisplayPort-4 --off --output DisplayPort-5 --off --output DisplayPort-6 --mode 1920x1080 --pos 3840x0 --rotate left --output DisplayPort-7 --mode 1920x1080 --pos 1920x357 --rotate normal --output DisplayPort-8 --off
